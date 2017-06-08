34c4f046e61c5432314935960fb6f77319202ff6
sofleal <sofislealferreira@gmail.com>
2017-06-08 12:32:57 -0300

    fim de aula

---
+++ aula20170608/hep3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+int main(){
+    char *texto= NULL;
+    char c;
+    int len=0;
+    printf("Digite um texto, e para sair digite '\\'+ENTER: \n");
+    do{
+        c=getchar();
+        len++;
+        texto =(char*)realloc(texto, len*sizeof(char));
+        texto[len-1]=toupper(c);
+    } while (c!= '\\');
+    texto[len-1]='\0';
+    printf("\n*************************\n\n");
+    printf ("%s", texto);
+    return 0;
+}
